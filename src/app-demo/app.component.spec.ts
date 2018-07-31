b9a3d7947289d646f1ab4e9a0fe7018d6c2a9f07
Mathilde <mathilde@onna.com>
2018-07-31 16:54:30 +0200

    fix unit tests

--- src/app-demo/app.component.spec.ts
+++ src/app-demo/app.component.spec.ts
@@ -1,18 +1,14 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {TrackingStorage} from "../lib/tracking/tracking-storage";
-import {Injectable, Type} from "@angular/core";
-import {TrackItDirective} from "../lib/tracking/track-it.directive";
-import {DefaultTrackingStorage} from "../lib/tracking/default-tracking-storage";
-import {TrackingModule} from "../lib/tracking/tracking.module";
-import {TrackItService} from "../lib/tracking/track-it.service";
-import {AppTrackingStorage} from "./app.tracking-storage";
-import {Router} from "@angular/router";
-import {TrackingSessionData} from "../lib/tracking/tracking-session-data.model";
-import {TrackingData} from "../lib/tracking/tracking-data.model";
-import {BrowserModule} from "@angular/platform-browser";
-import {APP_BASE_HREF} from "@angular/common";
-import {RouterTestingModule} from '@angular/router/testing';
+import { TrackingStorage } from '../lib/tracking/tracking-storage';
+import { Injectable } from '@angular/core';
+import { TrackingModule } from '../lib/tracking/tracking.module';
+import { TrackingSessionData } from '../lib/tracking/tracking-session-data.model';
+import { TrackingData } from '../lib/tracking/tracking-data.model';
+import { BrowserModule } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
 
 @Injectable()
 class MyTrackingStorage implements TrackingStorage {
@@ -31,35 +27,34 @@ class MyTrackingStorage implements TrackingStorage {
 
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-        imports: [
-            BrowserModule,
-            RouterTestingModule,
-            TrackingModule.storage(MyTrackingStorage)
-        ],
-      declarations: [
-          AppComponent
-      ],
-        providers: [{provide: 'TrackingStorage', useClass : MyTrackingStorage }, {provide: APP_BASE_HREF, useValue: '/'}]
-    }).compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                BrowserModule,
+                FormsModule,
+                RouterTestingModule,
+                TrackingModule.storage(MyTrackingStorage)
+            ],
+            declarations: [
+                AppComponent
+            ],
+            providers: [{provide: 'TrackingStorage', useClass: MyTrackingStorage}, {provide: APP_BASE_HREF, useValue: '/'}]
+        }).compileComponents();
+    }));
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+    it('should create the app', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    }));
 
-  it('should have a button testing area', async(() => {
-      const fixture = TestBed.createComponent(AppComponent);
-      //fixture.detectChanges();
-      const compiled = fixture.debugElement.nativeElement;
-      expect(compiled.querySelector('.button-events-test')).toBeTruthy();
-      // const app = fixture.debugElement.componentInstance;
-  }));
+    it('should have a button testing area', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('.button-events-test')).toBeTruthy();
+    }));
 
-  // If we click one of the buttons, the storage-service has to record it.
+    // If we click one of the buttons, the storage-service has to record it.
     it('should track a button click', async(() => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
@@ -81,15 +76,9 @@ describe('AppComponent', () => {
         expect(compiled.querySelector('.form-events-test')).toBeTruthy();
     }));
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  // it('should render title in a h1 tag', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  // }));
+    it(`should have as term 'app'`, async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app.term).toEqual('app');
+    }));
 });
